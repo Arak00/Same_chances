b8c6cd3b627bbf0b5630fd5845d4aa90b55faca3
audecvt <37926971+audecvt@users.noreply.github.com>
2018-06-26 18:15:51 +0200

    adapt routes for the two groups of users and signin/signup (#3)
    * adapt routes for the two groups of users and signin/signup
    
    * ruby gemset

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :recruiters
   devise_for :candidates
   root to: "home#index"
+  get '/signup', to: "home#signup"
+  get '/signin', to: "home#signin"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
