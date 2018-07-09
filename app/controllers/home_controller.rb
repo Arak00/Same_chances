2b89ff0f7d755d8bbca48fb7fb47829f7cf4886a
Aude Cervantes <aude.cervantes@hotmail.fr>
2018-07-09 16:23:30 +0200

    add first name and last name to sign up devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-<<<<<<< HEAD
   def index
   end
 
@@ -10,11 +9,4 @@ class HomeController < ApplicationController
 
   def signin
   end
-=======
-  def index; end
-
-  def signup; end
-
-  def signin; end
->>>>>>> master
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,9 @@
 Rails.application.routes.draw do
   devise_for :recruiters
   devise_for :candidates
-<<<<<<< HEAD
   root to: "home#index"
   get '/signup', to: "home#signup"
   get '/signin', to: "home#signin"
-=======
-  root to: 'home#index'
-  get '/signup', to: 'home#signup'
-  get '/signin', to: 'home#signin'
->>>>>>> master
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
