51331ba27be69dbf74521363e8b89d335a0fe4ee
Aude Cervantes <aude.cervantes@hotmail.fr>
2018-07-11 23:49:42 +0200

    new pages, homepage modified, new routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   root "home#index"
   get '/signup', to: "home#signup"
   get '/signin', to: "home#signin"
+  get '/about', to: "home#about", as: "/apropos"
+  get '/contact', to: "home#contact"
+  get '/offers', to: "home#offers"
+  get '/editcv', to: "home#editcv", as: "/editermoncv"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
